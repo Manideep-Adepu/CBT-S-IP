392aaf5d3aa7ef72c8d69145ce9aa03e1adaee2c
Manideep-Adepu <158537202+Manideep-Adepu@users.noreply.github.com>
2024-08-01 18:52:21 +0530

    Add build, export, and deploy scripts

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        domains: ['via.placeholder.com'],
-      },
+  output: 'export',
+  images: {
+      domains: ['via.placeholder.com'],
+  },
 };
-// next.config.js
-  
+
 export default nextConfig;
